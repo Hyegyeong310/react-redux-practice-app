cb201358ec08d5a4d6502bc60f958b0e054baddc
한혜경 <hyegyeong310@gmail.com>
2019-11-03 23:30:23 +0900

    [Add] project id route link 추가

--- src/components/projects/ProjectDetail.js
+++ src/components/projects/ProjectDetail.js
@@ -1,30 +1,54 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { firestoreConnect } from 'react-redux-firebase';
+import { compose } from 'redux';
 
 const ProjectDetail = props => {
+  const { project } = props;
+  if (project) {
+    return (
+      <div className="container section project-details">
+        <div className="card z-depth-0">
+          <div className="card-content">
+            <span className="card-title">{project.title}</span>
+            <p>{project.content}</p>
+          </div>
+          <div className="card-action grey lighten-4 grey-text">
+            <div>
+              Posted by The {project.authorFirstName} {project.authorLastName}
+            </div>
+            <div>3rd November, 5pm</div>
+          </div>
+        </div>
+      </div>
+    );
+  } else {
+    return (
+      <div className="container center">
+        <p>Loading project...</p>
+      </div>
+    );
+  }
+};
+
+const mapStateToProps = (state, ownProps) => {
   const {
     match: {
       params: { id }
     }
-  } = props;
-  return (
-    <div className="container section project-details">
-      <div className="card z-depth-0">
-        <div className="card-content">
-          <span className="card-title">Project Title - {id}</span>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores
-            amet velit error non nostrum officia incidunt, ea itaque! Id facere
-            distinctio dolore obcaecati saepe minus quisquam repellat doloremque
-            accusantium eveniet.
-          </p>
-        </div>
-        <div className="card-action grey lighten-4 grey-text">
-          <div>Posted by The Net Betty</div>
-          <div>3rd November, 5pm</div>
-        </div>
-      </div>
-    </div>
-  );
+  } = ownProps;
+  const {
+    firestore: {
+      data: { projects }
+    }
+  } = state;
+  const project = projects && projects[id];
+  return {
+    project
+  };
 };
 
-export default ProjectDetail;
+export default compose(
+  connect(mapStateToProps),
+  firestoreConnect([{ collection: 'projects' }])
+)(ProjectDetail);
