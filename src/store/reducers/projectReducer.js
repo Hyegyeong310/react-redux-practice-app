268624deb8cdb7035557366b67f9a5fb66fe9417
한혜경 <hyegyeong310@gmail.com>
2019-11-03 20:04:03 +0900

    [Add] redux 세팅

---
+++ src/store/reducers/projectReducer.js
@@ -0,0 +1,10 @@
+const initState = {};
+
+const projectReducer = (state = initState, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default projectReducer;
