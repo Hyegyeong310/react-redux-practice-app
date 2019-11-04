c4114f5bb32754edf1d0d7d58fb731925a80f7a9
한혜경 <hyegyeong310@gmail.com>
2019-11-04 18:23:32 +0900

    [Fix] 작성 일자 moment 모듈 포멧으로 수정

--- src/components/projects/ProjectDetail.js
+++ src/components/projects/ProjectDetail.js
@@ -3,6 +3,7 @@ import { connect } from 'react-redux';
 import { firestoreConnect } from 'react-redux-firebase';
 import { compose } from 'redux';
 import { Redirect } from 'react-router-dom';
+import moment from 'moment';
 
 const ProjectDetail = props => {
   const { auth, project } = props;
@@ -19,7 +20,7 @@ const ProjectDetail = props => {
             <div>
               Posted by The {project.authorFirstName} {project.authorLastName}
             </div>
-            <div>3rd November, 5pm</div>
+            <div>{moment(project.createdAt.toDate()).calendar()}</div>
           </div>
         </div>
       </div>
